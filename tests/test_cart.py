dc67329437875ecd68e3c398fa33b817f2872f44
Zahemass <93081795+Zahemass@users.noreply.github.com>
2025-01-01 17:31:06 +0530

    Update test_cart.py

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,5 +1,5 @@
 def test_cart_button_visibility(driver):
-    driver.get("https://www.saucedemo.com/")  # Navigate to the URL
+    driver.get("https://www.saucedemo.com/")  
     driver.find_element("id", "user-name").send_keys("standard_user")
     driver.find_element("id", "password").send_keys("secret_sauce")
     driver.find_element("id", "login-button").click()
@@ -9,19 +9,19 @@ def test_cart_button_visibility(driver):
     print("Button Visibility is completed")
   
 def test_add_multiple_items_to_cart(driver):
-    # Step 1: Navigate to the website
+   
     driver.get("https://www.saucedemo.com/")
     
-    # Step 2: Login to the website
+  
     driver.find_element("id", "user-name").send_keys("standard_user")
     driver.find_element("id", "password").send_keys("secret_sauce")
     driver.find_element("id", "login-button").click()
 
-    # Step 3: Add multiple items to the cart
+   
     driver.find_element("id", "add-to-cart-sauce-labs-backpack").click()
     driver.find_element("id", "add-to-cart-sauce-labs-bike-light").click()
 
-    # Step 4: Verify the cart badge shows "2"
+   
     cart_badge = driver.find_element("class name", "shopping_cart_badge")
     assert cart_badge.text == "2", "Failed to add multiple items to the cart."
     print("Multiple product adding test completed")
