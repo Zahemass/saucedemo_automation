e2c3d5a58da5c5ff8ed5bfbc8417da1318ffc432
Zahemass <93081795+Zahemass@users.noreply.github.com>
2025-01-01 17:27:34 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,14 +3,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import pytest
 
-DRIVER_PATH = r"C:\chrome driver\chromedriver-win64\chromedriver.exe"
+DRIVER_PATH = r" " # your chrome driver path
 
 @pytest.fixture
 def driver():
     options = Options()
-    # Comment out the headless mode to see the browser
-    # options.add_argument('--headless')
-    options.add_argument('--disable-gpu')  # Required for certain environments
+    options.add_argument('--disable-gpu') 
     service = Service(DRIVER_PATH)
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
