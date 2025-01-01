6ab8309aa4b8b60bec2cfc748920471a2b4f7efd
Zahemass <93081795+Zahemass@users.noreply.github.com>
2025-01-01 17:29:59 +0530

    Update test_logout.py

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -3,24 +3,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def test_logout(driver):
-    driver.get("https://www.saucedemo.com/")  # Navigate to the URL
+    driver.get("https://www.saucedemo.com/")  
 
     # Login
     driver.find_element(By.ID, "user-name").send_keys("standard_user")
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
     driver.find_element(By.ID, "login-button").click()
 
-    # Open the hamburger menu
+  
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.ID, "react-burger-menu-btn"))
     ).click()
 
-    # Wait for the logout link to be visible and click it
+   
     logout_link = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.ID, "logout_sidebar_link"))
     )
     logout_link.click()
 
-    # Validate redirection to the login page
+    
     assert driver.current_url == "https://www.saucedemo.com/", "Logout failed or URL mismatch."
     print("logout testing completed successfully.")
