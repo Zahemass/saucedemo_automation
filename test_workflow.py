da3b548d55bbd4cbdab59562b9083413dbc81687
Zahemass <93081795+Zahemass@users.noreply.github.com>
2025-01-03 21:11:32 +0530

    Update test_workflow.py

--- test_workflow.py
+++ test_workflow.py
@@ -1,62 +1,23 @@
-import time
+import pytest
 from selenium import webdriver
+from utils.config import BASE_URL, DRIVER_PATH
+from pages.login_page import LoginPage
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import random
 
-# Path to your chromedriver
-DRIVER_PATH = r" " # your chrome driver path
-
-# Driver fixture
-def setup_driver():
+@pytest.fixture
+def driver():
     options = Options()
-    options.add_argument("--disable-gpu")
+    options.add_argument("--start-maximized")
     service = Service(DRIVER_PATH)
     driver = webdriver.Chrome(service=service, options=options)
-    return driver
+    yield driver
+    driver.quit()
 
 
-# Test function
-def test_automation_workflow():
-    driver = setup_driver()
-    
-    try:
-        driver.get("https://www.saucedemo.com/")
-        time.sleep(2)  # Observe the login page
-
-        
-        driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        driver.find_element(By.ID, "login-button").click()
-        time.sleep(2)  # Observe the product page
-
-        
-        product_buttons = driver.find_elements(By.CLASS_NAME, "btn_inventory")
-        selected_products = random.sample(product_buttons, 4)
-        for product in selected_products:
-            product.click()
-            time.sleep(1)  # Observe each product addition
-
-       
-        driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-        time.sleep(2)  # Observe the cart page
-
-        
-        cart_items = driver.find_elements(By.CLASS_NAME, "cart_item")
-        assert len(cart_items) == 4, f"Expected 4 items in the cart, but found {len(cart_items)}."
-        print("add random products verification test")
-
-       
-        driver.find_element(By.ID, "react-burger-menu-btn").click()
-        time.sleep(1)  # Observe the menu opening
-        driver.find_element(By.ID, "logout_sidebar_link").click()
-        time.sleep(2)  # Observe the logout process
-
-    finally:
-        driver.quit()
-
-
-# Execute the test
-if __name__ == "__main__":
-    test_automation_workflow()
+def test_login_standard_user(driver):
+    driver.get(BASE_URL)
+    login_page = LoginPage(driver)
+    login_page.login("guvi_user", "Secret@123")
+    assert "inventory" in driver.current_url
+    print("login testing completed successfully.")
