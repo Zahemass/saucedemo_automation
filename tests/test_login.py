1947567c0b86cabdfc0f55e5b4a57c9f695022ab
Zahemass <93081795+Zahemass@users.noreply.github.com>
2025-01-03 21:11:05 +0530

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,6 +4,7 @@ from utils.config import BASE_URL, DRIVER_PATH
 from pages.login_page import LoginPage
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 @pytest.fixture
 def driver():
@@ -14,10 +15,26 @@ def driver():
     yield driver
     driver.quit()
 
-
-def test_login_standard_user(driver):
+@pytest.mark.parametrize("username", [
+    "standard_user", 
+    "problem_user", 
+    "performance_glitch_user", 
+    "locked_out_user"
+])
+def test_login_various_users(driver, username):
     driver.get(BASE_URL)
     login_page = LoginPage(driver)
-    login_page.login("standard_user", "secret_sauce")
-    assert "inventory" in driver.current_url
-    print("login testing completed successfully.")
+    login_page.login(username, "secret_sauce")
+    if username == "locked_out_user":
+        error_message = driver.find_element(By.CLASS_NAME, "error-message-container").text
+        assert "locked out" in error_message.lower()
+    else:
+        assert "inventory" in driver.current_url
+        print(username,"Successfully logged")
+
+def test_login_using_cookies(driver):
+    driver.get(BASE_URL)
+    driver.add_cookie({"name": "session-id", "value": "mock-session-id", "path": "/"})
+    driver.refresh()
+    assert "inventory" in driver.current_url 
+
